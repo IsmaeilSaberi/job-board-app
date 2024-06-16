b4eae7598470537259cb744789de93364aa84dbe
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-06-15 23:07:39 -0700

    First commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="container flex items-center justify-between py-4 px-6 mx-auto">
+        <Link className="font-bold text-xl" href={"/"}>
+          Job Board
+        </Link>
+        <nav className="flex gap-2  *:py-2 *:px-4 *:rounded-md">
+          <Link className="bg-gray-200" href={"/login"}>
+            Login
+          </Link>
+          <Link className="bg-blue-500 text-white" href={"/new-listing"}>
+            Post a job
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
